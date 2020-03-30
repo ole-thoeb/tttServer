55ad9c4d8ed70cd2f70391937d8b258cfb0ccb8c
ole_thoeb <eloemil@yahoo.de>
2020-03-30 19:12:07 +0200

    default mode is production

--- src/Application.kt
+++ src/Application.kt
@@ -25,7 +25,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = true) {
+fun Application.module(testing: Boolean = false) {
     val tttGameServer = TTTGameServer(this, testing)
     install(CallLogging) {
         level = Level.INFO
